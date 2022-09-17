688c6c1e34ebf3051387af133907da44235836aa
Rodolfo Roman <rromanv@gmail.com>
2022-09-17 17:32:41 -0500

    feat(pinia): added pinia store

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,1 +1,22 @@
-<template>Home page</template>
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <h1>Home Page</h1>
+  <pre>The counter is: {{ counter.count }}</pre>
+  <div class="mini-wrapper">
+    <button @click="counter.increment">Increment</button>
+    <button @click="counter.decrement">Decrement</button>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.mini-wrapper {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+}
+</style>
---
+++ src/stores/counter.ts
@@ -0,0 +1,21 @@
+import { defineStore, acceptHMRUpdate } from 'pinia'
+
+export const useCounterStore = defineStore('counter', {
+  state: () => {
+    return { count: 0 }
+  },
+  // could also be defined as
+  // state: () => ({ count: 0 })
+  actions: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    },
+  },
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot))
+}
